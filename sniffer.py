b477ef015143fdeb485faf8ee0b762fbad59f981
AuxGrep <103135612+AuxGrep@users.noreply.github.com>
2022-09-25 11:36:12 +0300

    Add files via upload

---
+++ sniffer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#Written_By_Auxgrep
+
+import time
+import os
+
+def cmd():
+
+	target_list = input("Enter the target Domain(eg. facebook.com): ")
+	time.sleep(3)
+	print("")
+	data_con = target_list
+	limit = "/url_list?limit=200&page=1"
+	char2 = limit[0]
+	cmdx = os.system('curl -s "https://otx.alienvault.com/api/v1/indicators/domain/{}{}"| jq -r .url_list[].url'\
+	.format(data_con, limit))
+cmd();
+
