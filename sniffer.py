beb63dff736ce72c33ca99642bcf2e9a2f0ba21a
AuxGrep <103135612+AuxGrep@users.noreply.github.com>
2022-09-27 16:01:12 +0300

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -6,13 +6,13 @@ import os
 
 def cmd():
 
-	target_list = input("Enter the target Domain(eg. facebook.com): ")
-	time.sleep(3)
-	print("")
-	data_con = target_list
-	limit = "/url_list?limit=200&page=1"
-	char2 = limit[0]
-	cmdx = os.system('curl -s "https://otx.alienvault.com/api/v1/indicators/domain/{}{}"| jq -r .url_list[].url'\
-	.format(data_con, limit))
+        target_list = input("Enter the target Domain(eg. facebook.com): ")
+        time.sleep(2)
+        print("")
+        data_con = target_list
+        limit = "/url_list?limit=200&page=1"
+        char2 = limit[0]
+        cmdx = os.system('curl -s "https://otx.alienvault.com/api/v1/indicators/domain{}{}{}"| jq -r .url_list[].url'\
+        .format(char2, data_con, limit))
 cmd();
 
