8fb82919b9afdb1a077c99a3bc9a5bcdc283fc9d
AuxGrep <103135612+AuxGrep@users.noreply.github.com>
2023-01-05 19:51:49 +0300

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -3,16 +3,28 @@
 
 import time
 import os
+import sys
+import platform
+
 
 def cmd():
 
-        target_list = input("Enter the target Domain(eg. facebook.com): ")
-        time.sleep(2)
-        print("")
-        data_con = target_list
-        limit = "/url_list?limit=200&page=1"
-        char2 = limit[0]
-        cmdx = os.system('curl -s "https://otx.alienvault.com/api/v1/indicators/domain{}{}{}"| jq -r .url_list[].url'\
-        .format(char2, data_con, limit))
+        try:
+                if platform.system() != 'Linux':
+                        print('Script works with Linux Only')
+                        sys.exit()
+                else:
+                        target_list = str(sys.argv[1])
+                        time.sleep(1)
+                        data_con = target_list
+                        limit = "/url_list?limit=200&page=1"
+                        char2 = limit[0]
+                        cmdx = os.system('curl -s "https://otx.alienvault.com/api/v1/indicators/domain{}{}{}"| jq -r .url_list[].url'\
+                        .format(char2, data_con, limit))
+        except OSError:
+                print('Unkown Error occured!! exiting')
+                sys.exit()
 cmd();
+
+
 
